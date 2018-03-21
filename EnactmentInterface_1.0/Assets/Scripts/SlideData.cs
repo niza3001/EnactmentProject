ecfc6ae12853dbf1f273ac1c0acea33af1cd4a8e
Sarah Brown <brownsarah@tamu.edu>
2018-03-21 08:19:53 -0500

    Enactment Interface Upload
    Initial upload of the new Enactment Interface

---
+++ EnactmentInterface_1.0/Assets/Scripts/SlideData.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlideData : MonoBehaviour {
+
+    /*IN PROGRESS - this is a script attached to each slide to record which index of each object type it holds, as well as its recording data*/
+
+    private AudioClip slideClip;
+    private int charaIndex;
+    private int backdropIndex;
+    private int itemIndex;
+    private AudioSource slideAudio;
+
+	// Use this for initialization
+	void Start () {
+        slideAudio = gameObject.AddComponent<AudioSource>();
+        slideClip = new AudioClip();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void startRecord()
+    {
+        slideAudio.clip = Microphone.Start(null, true, 300, 44100);
+        Debug.Log("We have started");
+    }
+
+    public void endRecord()
+    {
+        int timeCut = Microphone.GetPosition(null);
+        Microphone.End(null);
+
+        float[] samples = new float[timeCut];
+        slideAudio.clip.GetData(samples, 0);
+
+
+        int freq = slideAudio.clip.frequency;
+        slideAudio.clip = AudioClip.Create("SlideSound", samples.Length, 1, freq, false);
+        slideAudio.clip.SetData(samples, 0);
+
+        Debug.Log("We have stopped");
+
+        if (slideAudio.clip == null) { Debug.Log("uuhm"); }
+
+    }
+
+    public void playAudio()
+    {
+        if (slideAudio.clip != null) {
+            Debug.Log("well something is playing");
+            slideAudio.Play();
+           
+        }
+    }
+
+    public void setChara(int ind)
+    {
+        charaIndex = ind;
+    }
+
+    public void setBackdrop(int ind)
+    {
+        backdropIndex = ind;
+    }
+
+    public void setItem(int ind)
+    {
+        itemIndex = ind;
+    }
+
+    public int getChara()
+    {
+        return charaIndex;
+    }
+
+    public int getBackdrop()
+    {
+        return backdropIndex;
+    }
+
+    public int getItem()
+    {
+        return itemIndex;
+    }
+}
---
+++ EnactmentInterface_1.0/Assets/Scripts/SlideNumbering.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class SlideNumbering : MonoBehaviour
+{
+
+    /*This class updates the numbers for each slide according to their position in the heirarchy, working its way down from the Beginning slide array to the Ending slides*/
+    /*For this reason, it's important to keep the slides in order in the heirarchy by their id in each array*/
+
+    private bool isRecording = false;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        int num = 1;
+
+        foreach (Transform child in this.transform) //Gets all slide arrays set as children to the GameObject with this script
+        {
+            foreach (Transform grandchild in child.transform)
+            {
+                if (grandchild.GetComponentInChildren<SlideSelectSlide>() != null) //Since the add slide buttons and slides are under the same parent, only objects with a Slide script is assigned a number to its Text component
+                {
+                    if (grandchild.GetComponentInChildren<Text>() != null)
+                    {
+                        grandchild.GetComponentInChildren<Text>().text = num.ToString();
+                        num++;
+                    }
+                }
+            }
+        }
+
+    }
+
+
+    //deselects all slides across all three arrays so only one slide is selected
+    //is called by SelectMe in SlideSelectSlide script, by the slide being selected
+    public void selectNew(int section, int slide) 
+    {
+        SlideArray[] children = GetComponentsInChildren<SlideArray>();
+
+        for (int i = 0; i < children.Length; i++)
+        {
+            SlideSelectSlide[] grandchildren = children[i].GetComponentsInChildren<SlideSelectSlide>();
+
+            for (int k = 0; k < grandchildren.Length; k++)
+            {
+                if (i == section && grandchildren[k].getListID() == slide)
+                {
+                    //do nothing, the slide calling this function handles its own state
+                }
+                else { grandchildren[k].deselectMe(); }
+
+            }
+        }
+
+    }
+
+
+    //goes through all slides when the delete button is pressed to find which one is selected for deletion
+    public void deleteSlide()
+    {
+        SlideArray[] children = GetComponentsInChildren<SlideArray>();
+
+        for (int i = 0; i < children.Length; i++)
+        {
+            SlideSelectSlide[] grandchildren = children[i].GetComponentsInChildren<SlideSelectSlide>();
+
+            for (int k = 0; k < grandchildren.Length; k++)
+            {
+                if (grandchildren[k].getSelected() == true) { grandchildren[k].deleteMe(); }
+
+            }
+        }
+
+
+    }
+
+
+    public void recordSelectedSlide()
+    {
+
+        SlideArray[] children = GetComponentsInChildren<SlideArray>();
+
+        for (int i = 0; i < children.Length; i++)
+        {
+            SlideSelectSlide[] grandchildren = children[i].GetComponentsInChildren<SlideSelectSlide>();
+            SlideData[] grandchildrenData = children[i].GetComponentsInChildren<SlideData>();
+            for (int k = 0; k < grandchildren.Length; k++)
+            {
+                if (grandchildren[k].getSelected() == true) {
+
+
+                    if (isRecording == false)
+                    {
+                        grandchildrenData[k].startRecord();
+                        isRecording = true;
+                    }
+                    else
+                    {
+                        grandchildrenData[k].endRecord();
+                        isRecording = false;
+                    }
+
+
+                }
+
+            }
+        }
+
+    }
+
+    public void playSelectedSlide()
+    {
+        SlideArray[] children = GetComponentsInChildren<SlideArray>();
+
+        for (int i = 0; i < children.Length; i++)
+        {
+            SlideSelectSlide[] grandchildren = children[i].GetComponentsInChildren<SlideSelectSlide>();
+            SlideData[] grandchildrenData = children[i].GetComponentsInChildren<SlideData>();
+            for (int k = 0; k < grandchildren.Length; k++)
+            {
+                if (grandchildren[k].getSelected() == true)
+                {
+
+                    
+                        grandchildrenData[k].playAudio();
+                    
+
+
+                }
+
+            }
+        }
+    }
+
+}
